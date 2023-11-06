ecb66db9d6b2c80f5cf83d824bec103bf2013c6b
leroygatlin <leroygatlin@gmail.com>
2023-11-06 13:46:24 -0800

    complete functions continued

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+     return (`This Saturday, I want to ${activity}!`)
+    }
+    
+    console.log(saturdayFun("Pick up my son"))
+    console.log(saturdayFun())
+    
+    function mondayWork(activity = 'go to the office'){
+    
+        return (`This Monday, I will ${activity}.`)
+    }
+    
+    function wrapAdjective(symbol = '*'){
+        return function innerfunnction(adjective = 'special'){
+    
+    
+            return `You are ${symbol}${adjective}${symbol}!`
+        }
+    }
+    
+    
