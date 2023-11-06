0e48d692106caa313fa837b9650584647391c703
leroygatlin <leroygatlin@gmail.com>
2023-11-06 13:31:47 -0800

    commit tree e8de9a3939a8d7901e6da59eb771f066a625fc01

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+
+    return (`This Saturday, I want to ${activity}!`)
+    }
+    
+    console.log(saturdayFun("Pick up my son"))
+    console.log(saturdayFun())
+    
+    function mondayWork(activity = 'go to the office'){
+    
+        return (`This Monday, I will ${activity}.`)
+    }
+    
+    // console.log(mondayWork("work from home"))
+    
+    // function outer(statement, msg = 'You are !'){
+        // const innerfunction = function (compliment = "special")}
+        // return `You are ${compliment}!`;
+    // };
+    // return innerfunction;
+    
+    // }
+    
+    function wrapAdjective(symbol = '*'){
+        return function innerfunnction(adjective = 'special'){
+    
+    
+            return `You are ${symbol}${adjective}${symbol}!`
+        }
+    }
+    
+    console.log(wrapAdjective()());
+    console.log(wrapAdjective('^')('silly'));
